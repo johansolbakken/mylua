8e43f55d91998efca624788bd1dd6e4678e1e9a9
Johan Solbakken <johsol@vespa.ai>
2026-07-28 20:14:16 +0200

    more text

--- startup.lua
+++ startup.lua
@@ -3,3 +3,16 @@ term.setCursorPos(1,1)
 write("Login. Who are you? ")
 name = read()
 print("Hello " .. name .. ", welcome to computer!")
+
+
+
+local monitor = peripheral.wrap("right")
+
+monitor.clear()
+monitor.setCursorPos(1, 1)
+monitor.write("Hello brothers!")
+monitor.setCursorPos(1, 2)
+monitor.write("We are going the the moon!")
+monitor.setCursorPos(1, 4)
+monitor.write("RIP Gust Giovanni von Grevenstein zzZ")
+
